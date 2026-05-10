9e5a1482b1794e7c29164afbedd16d244db3cd4a
Uva-nana <uvananaj1l@gmail.com>
2026-05-10 09:49:09 +0530

    updated

---
+++ Leetcode/src/Twopointers/ReverseString.java
@@ -0,0 +1,24 @@
+package Twopointers;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		char [] s = {'h','e','l','l','o'};
+		// How will You reverse this 
+		int left = 0;
+		int right = s.length-1;
+		char temp ; 
+	
+	if (left <right) {
+		char temp = s[left];
+		
+		
+	}
+		
+		
+		 
+		
+		
+	}
+
+}
